b211027089d2874e4162abe1cba3d0f31e1dee15
aneme1038 <thriftman25@gmail.com>
2020-02-25 17:57:37 -0500

    updated views.py

--- azure/projectA/views.py
+++ azure/projectA/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Users
+from .models import Item
 # Create your views here.
-# def index(response):
-users = Users.objects.all()
-for user in users:
-    print user.username_text
+
+#Get items and display them
+def index(request):
+    #get the latest published Item in your list
+    latest_item_list = Item.objects.order_by('-creation_date')[:5]
+    #put the list into an object for contextual display
+    context = {'latest_item': latest_item}
+    return render(request, 'projectA/index.html', context)
+
+#Show specific item and its content
+def item_info(request, item_id):
+    try:
+        item = Item.objects.get(pk=item_id)
+    except Item.DoesNotExist:
+        raise Http404("Item Does Not Exist")
+    return render(request, 'projectA/InsertUrlPathwayHere', { 'item': item})
+
+#Create other views here 
