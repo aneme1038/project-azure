5f4833a5cc9368fb884973023502252dbdd7b46c
aneme1038 <thriftman25@gmail.com>
2020-03-10 11:34:12 -0400

    added django rest framework

--- azure/azure/settings.py
+++ azure/azure/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myapp.middleware.dev_cors_middleware'
 ]
 
 ROOT_URLCONF = 'azure.urls'
--- azure/projectA/views.py
+++ azure/projectA/views.py
@@ -11,7 +11,7 @@ def index(request):
     #get the latest published Item in your list
     latest_item_list = Item.objects.order_by('-creation_date')[:5]
     #put the list into an object for contextual display
-    template = loader.get_template('projectA/index.html')
+    template = loader.get_template('project-azure/public/index.html')
     context = {'latest_item': latest_item_list}
     return HttpResponse(template.render(context, request))
 
