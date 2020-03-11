022b7744a8a1ffac06a369b1ba91ee9d69a2ce97
aneme1038 <thriftman25@gmail.com>
2020-03-11 08:56:30 -0400

    added frontend navbar. still cannot render new code

--- azure/frontend/src/components/app.js
+++ azure/frontend/src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Header from './layout/Header';
 
 class App extends Component {
   render() {
     return (
-      <h1>React App</h1>,
-      <h3>hi there</h3>
+      <Header />
     )
   }
 }
