c145278497a9c3d901cb1fc5e98e360975721ef6
aneme1038 <thriftman25@gmail.com>
2020-03-23 13:36:27 -0400

    added a bunch of new files and folders for setup

---
+++ azure/frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+//==========================================//
+//---------------HEADER--------------------//
+//The Header will only contain the title  //
+//=======================================//
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <h1>Azure</h1>
+    )
+  }
+}
+
+export default Header
--- azure/frontend/src/components/app.js
+++ azure/frontend/src/components/app.js
@@ -1,6 +1,11 @@
+//==========================================//
+//---------------APP-----------------------//
+//    This is the main container for all  //
+//          React Components             //
+//======================================//
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Header from './layout/Header';
+import Header from './Header';
 
 class App extends Component {
   render() {
