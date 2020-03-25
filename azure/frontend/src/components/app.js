01b0c1710de37e33e0ccb388dacb0eb665621ed8
aneme1038 <thriftman25@gmail.com>
2020-03-25 17:20:08 -0400

    updated package-lock.json

--- azure/frontend/src/components/app.js
+++ azure/frontend/src/components/app.js
@@ -8,8 +8,21 @@ import ReactDOM from 'react-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Navigation from './Navigation';
+require('dotenv').config();
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      view: {
+        page: 'home',
+        pageTitle: ''
+      },
+    }
+    //this.handleBLANK = this.handleBLANK.bind(this)
+  }
+  // ============== //
+  // HANDLERS      //
   render() {
     return (
       <Header />
