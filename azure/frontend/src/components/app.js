e0c576c7f1206119123ba670b871fb06e71dcd14
aneme1038 <thriftman25@gmail.com>
2020-03-25 17:48:54 -0400

    added new file. updated header.js. added some handlers to app.js

--- azure/frontend/src/components/app.js
+++ azure/frontend/src/components/app.js
@@ -23,6 +23,75 @@ class App extends Component {
   }
   // ============== //
   // HANDLERS      //
+  // ============== //
+  // Handle View State
+  handleView = (view, postData) => {
+    let pageTitle = ''
+    let userInputs = {
+      username: '',
+      password: '',
+      id: null
+    }
+    // decide pageTitle based on view
+    switch (view) {
+      case 'home':
+        pageTitle = 'Home'
+        break
+      case 'finance':
+        pageTitle = 'Finance'
+        break
+      case 'health':
+        pageTitle = 'Health'
+        break
+      case 'religion':
+        pageTitle = 'Religion'
+        break
+      case 'statistics':
+        pageTitle = 'My Statistics'
+        break
+      case 'food':
+        pageTitle = 'Diet and Nutrition'
+        break
+      case 'shopping':
+        pageTitle = 'Shopping'
+        break
+      case 'education':
+        pageTitle = 'Education'
+        break
+      case 'scheduler':
+        pageTitle = 'Personal Planner'
+        break
+      case 'settings':
+        pageTitle = 'Settings'
+        break
+      default:
+        break
+    }
+    //Update state
+    this.setState({
+      view: {
+        page: view,
+        pageTitle: pageTitle
+      },
+      userInputs: userInputs
+    })
+  }
+  //Handle Search
+  // handleSearch(event) {
+  //   console.log('This Route is Working');
+  //   event.preventDefault()
+  //   this.setState({
+  //
+  //   }, () => {
+  //
+  //   })
+  // }
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+  // =========== //
+  // RENDER     //
+  // ========== //
   render() {
     return (
       <Header />
